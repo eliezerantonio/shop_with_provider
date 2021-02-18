1b39254429a6fd0b7bd44591bcabcd3a98a88577
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-02-18 03:18:23 +0100

    add products

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Minha Loja',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
       home: ProductsScreen(),
     );
   }
 }
-
