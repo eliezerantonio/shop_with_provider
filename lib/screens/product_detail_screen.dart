90af0c4b5acbefa3ee7b7b01c73f4e550fb0d2e4
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-02-19 01:54:38 +0100

    detalhes do produto

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:gerencimento_estado/providers/product.dart';
 import 'package:gerencimento_estado/providers/counter_provider.dart';
@@ -8,12 +10,43 @@ class ProductDetailScreen extends StatelessWidget {
     final Product proudct =
         ModalRoute.of(context).settings.arguments as Product;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(proudct.title),
-      ),
-      body: Column(
-        children: [],
-      ),
-    );
+        appBar: AppBar(
+          title: Text(proudct.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  proudct.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "AOA ${proudct.price}",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                width: double.infinity,
+                child: Text(
+                  proudct.description,
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
