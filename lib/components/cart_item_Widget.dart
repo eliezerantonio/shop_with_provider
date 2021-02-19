306eb06bffa51e9404106e42ce673604566c6d6c
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-02-19 01:47:33 +0100

    exluindo com Dismissible

--- lib/components/cart_item_Widget.dart
+++ lib/components/cart_item_Widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:gerencimento_estado/providers/cart.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -7,22 +8,40 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: EdgeInsets.all(5),
-              child: FittedBox(
-                child: Text("${cartItem.price}"),
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        padding: EdgeInsets.only(right: 20),
+        alignment: Alignment.centerRight,
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      onDismissed: (_) {
+        context.read()<Cart>().removeItem(cartItem.productId);
+      },
+      direction: DismissDirection.endToStart,
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text("${cartItem.price}"),
+                ),
               ),
             ),
+            title: Text(cartItem.title),
+            subtitle: Text("Total: AOA ${cartItem.price * cartItem.quantity}"),
+            trailing: Text("${cartItem.quantity}"),
           ),
-          title: Text(cartItem.title),
-          subtitle: Text("Total: AOA ${cartItem.price * cartItem.quantity}"),
-          trailing: Text("${cartItem.quantity}"),
         ),
       ),
     );
