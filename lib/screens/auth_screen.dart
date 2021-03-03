6777027d7bcedfdc5242adb043f7764dcee7ba80
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-03 12:17:56 +0100

    tela part2 logo

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatelessWidget {
@@ -23,8 +25,34 @@ class AuthScreen extends StatelessWidget {
           Container(
             width: double.infinity,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 70,
+                  ),
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.deepOrange.shade900,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                        )
+                      ]),
+                  child: Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      color: Theme.of(context).accentTextTheme.headline6.color,
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                    ),
+                  ),
+                )
               ],
             ),
           )
