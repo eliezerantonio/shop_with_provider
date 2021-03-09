c6f5df9c1d52a76b9ee6da352b736fe1708adec5
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-09 23:54:01 +0100

    REMOVENDO KEY

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
+import 'package:gerencimento_estado/const/token.dart';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final _url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDO9FOhXzYndsm0cWEOZeIsLYdFsaa5RAQ';
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=${Consts.TOKEN}';
 
     final response = await http.post(
       _url,
