5debd154caf42c7ee3bae0157feac5dbe0fdfa46
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-11 02:34:35 +0100

    animatedBuilder

--- lib/components/auth_card.dart
+++ lib/components/auth_card.dart
@@ -32,7 +32,7 @@ class _AuthCardState extends State<AuthCard>
         .animate(
             CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn));
 
-    _heightAnimation.addListener(() => setState(() {}));
+    // _heightAnimation.addListener(() => setState(() {}));
   }
 
   Map<String, String> _authData = {'email': '', 'password': ''};
@@ -111,94 +111,100 @@ class _AuthCardState extends State<AuthCard>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(30),
       ),
-      child: Container(
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16),
-        //  height: _authMode == AuthMode.Login ? 329 : 400,
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "E-mail",
-                  labelStyle: TextStyle(fontSize: 22),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (context, ch)=> Container(
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16),
+          //  height: _authMode == AuthMode.Login ? 329 : 400,
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          child: ch,
+        ),
+        child:  Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "E-mail",
+                    labelStyle: TextStyle(fontSize: 22),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value.trim().isEmpty || !value.contains("@")) {
+                      return 'Informe um e-mail valido';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _authData["email"] = value,
                 ),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value.trim().isEmpty || !value.contains("@")) {
-                    return 'Informe um e-mail valido';
-                  }
-                  return null;
-                },
-                onSaved: (value) => _authData["email"] = value,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                obscureText: true,
-                controller: _passwordControler,
-                decoration: InputDecoration(
-                  labelText: "Senha",
-                  labelStyle: TextStyle(fontSize: 22),
+                SizedBox(
+                  height: 10,
                 ),
-                keyboardType: TextInputType.text,
-                validator: (value) {
-                  if (value.trim().isEmpty || value.length < 6) {
-                    return 'Informe um a senha';
-                  }
-                  return null;
-                },
-                onSaved: (value) => _authData["password"] = value,
-              ),
-              if (_authMode == AuthMode.Singup)
                 TextFormField(
                   obscureText: true,
+                  controller: _passwordControler,
                   decoration: InputDecoration(
-                    labelText: "Confirmar senha",
+                    labelText: "Senha",
                     labelStyle: TextStyle(fontSize: 22),
                   ),
                   keyboardType: TextInputType.text,
-                  validator: _authMode == AuthMode.Singup
-                      ? (value) {
-                          if (value != _passwordControler.text) {
-                            return 'Senha nao confere';
+                  validator: (value) {
+                    if (value.trim().isEmpty || value.length < 6) {
+                      return 'Informe um a senha';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _authData["password"] = value,
+                ),
+                if (_authMode == AuthMode.Singup)
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Confirmar senha",
+                      labelStyle: TextStyle(fontSize: 22),
+                    ),
+                    keyboardType: TextInputType.text,
+                    validator: _authMode == AuthMode.Singup
+                        ? (value) {
+                            if (value != _passwordControler.text) {
+                              return 'Senha nao confere';
+                            }
+                            return null;
                           }
-                          return null;
-                        }
-                      : null,
-                ),
-              Spacer(),
-              if (_isLoading)
-                CircularProgressIndicator()
-              else
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                  color: Theme.of(context).primaryColor,
-                  textColor: Theme.of(context).primaryTextTheme.button.color,
-                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 14),
+                        : null,
+                  ),
+                Spacer(),
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).primaryTextTheme.button.color,
+                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 14),
+                    child: Text(
+                        _authMode == AuthMode.Login ? "Entrar" : "Registrar"),
+                    onPressed: _submit,
+                  ),
+                Spacer(),
+                FlatButton(
+                  onPressed: _switchAuthMode,
                   child: Text(
-                      _authMode == AuthMode.Login ? "Entrar" : "Registrar"),
-                  onPressed: _submit,
-                ),
-              Spacer(),
-              FlatButton(
-                onPressed: _switchAuthMode,
-                child: Text(
-                  "Alternar p/${_authMode == AuthMode.Login ? "Registrar" : "Entrar"}"
-                      .toUpperCase(),
-                  style: TextStyle(fontSize: 16),
-                ),
-                textColor: Theme.of(context).primaryColor,
-              )
-            ],
+                    "Alternar p/${_authMode == AuthMode.Login ? "Registrar" : "Entrar"}"
+                        .toUpperCase(),
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  textColor: Theme.of(context).primaryColor,
+                )
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        
+        )
+      );
+    
   }
 }
